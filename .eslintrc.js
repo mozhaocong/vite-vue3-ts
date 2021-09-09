7ce3b89b20bce4b013f6339b8bc3cf58ec40fc50
mozhaoocng <18825060695@163.com>
2021-09-09 17:07:44 +0800

    feature：esline规则优化

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,48 @@
 module.exports = {
-	parser: 'vue-eslint-parser',
-	parserOptions: {
-		parser: '@typescript-eslint/parser',
-		ecmaVersion: 2020,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-	env: {
-		node: true,
-		es6: true,
-		browser: true,
-	},
-	globals: {
-		Markdown: true,
-	},
-	extends: [
-		'plugin:vue/vue3-recommended',
-		'plugin:@typescript-eslint/recommended',
-		'prettier/@typescript-eslint',
-		'plugin:prettier/recommended',
-		'eslint:recommended',
-	],
-	rules: {
-		'prettier/prettier': [
-			'error',
-			{ endOfLine: 'auto' },
-			{
-				usePrettierrc: true,
-				tabWidth: 2,
-				useTabs: true,
-			},
-		],
-	},
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  env: {
+    node: true,
+    es6: true,
+    browser: true,
+  },
+  globals: {
+    Markdown: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      { endOfLine: 'auto' },
+      {
+        usePrettierrc: true,
+        tabWidth: 2,
+        useTabs: true,
+      },
+    ],
+    'vue/max-attributes-per-line': [
+      'warn',
+      {
+        singleline: 10,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+    'vue/singleline-html-element-content-newline': 0,
+  },
 }
--- src/views/test/index.tsx
+++ src/views/test/index.tsx
@@ -1,11 +1,11 @@
-import {defineComponent} from 'vue'
-export default  defineComponent({
-    name: 'testMZC',
-    setup() {
-        return () =>
-            <div>
-                <div>78945123</div>
-            </div>
-
-    }
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'TestMZC',
+  setup() {
+    return () => (
+      <div>
+        <div>78945123</div>
+      </div>
+    )
+  },
 })
