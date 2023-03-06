5e40605bbfce4f694602a3048ed3a77515919b0b
mozhaocong <>
2023-03-06 18:40:07 +0800

    更新

--- src/views/knife4j/_module/index.tsx
+++ src/views/knife4j/_module/index.tsx
@@ -1,5 +1,7 @@
 // 处理knife4j文档返回的数据
 
+import { isTrue, deepClone, getArrayReduceObject } from 'html-mzc-tool'
+
 function setDataObject(item: { list: string[]; setData: any; data: any }): void {
 	const { list, setData, data } = item
 	let psuData = setData
@@ -48,4 +50,124 @@ export function setKnife4jApiData(item: { data: any }) {
 		listData.push({ tag: pathDataKey, children: itemList })
 	}
 	return listData
+}
+
+// 获取目标数据
+export function getTargetApiData(item: { pathList: any[]; apiData: ObjectMap; targetItem: ObjectMap }) {
+	const { pathList, apiData, targetItem } = item
+	const { tag, path, methods } = targetItem
+	let findData = {}
+	for (const forItem of pathList) {
+		const { tag: forTag, children } = forItem
+		if (forTag === tag) {
+			findData = children.find((findItem) => {
+				return path === findItem.path && methods === findItem.methods
+			})
+			break
+		}
+	}
+	if (!isTrue(findData)) {
+		return {}
+	}
+	const targetData = deepClone(findData)
+	getRequestBodyData({ targetData, apiData })
+	// console.log('findData', targetData, apiData)
+}
+
+// requestBody 类型处理，可以识别出有没有漏掉要补的类型，有检查的作用
+const requestBodyType = {
+	'application/json': { route: ['schema', '$ref'], method: schemaRefMethod }
+}
+
+const apiTypeMap = {
+	integer: 'number',
+	array: 'array',
+	string: 'string'
+}
+
+function schemaRefMethod(item: { apiData: any; routeString: string }): ObjectMap {
+	const data = refMethod(item)
+	console.log('data', data)
+	const { properties, required = [] } = data
+	const listData = []
+	for (const property in properties) {
+		const sourceData = properties[property]
+		const { type, description } = sourceData
+
+		// 其他参数处理
+		const otherData: ObjectMap = {}
+		if (sourceData.enum) {
+			otherData.enum = sourceData.enum
+		}
+		if (required.includes(property)) {
+			otherData.required = true
+		}
+		listData.push({ ...otherData, key: property, description, type: apiTypeMap[type] })
+	}
+	console.log(listData)
+	return {}
+}
+
+function getPropertiesData(item: { apiData: any; routeString: string }) {
+	const data = refMethod(item)
+	const { properties, required = [] } = data
+	const listData = []
+	for (const property in properties) {
+		// 其他参数处理
+		const pushData: ObjectMap = getPropertiesKeyData(properties[property])
+		if (required.includes(property)) {
+			pushData.required = true
+		}
+		listData.push({ ...pushData, key: property })
+	}
+	console.log(listData)
+	return listData
+}
+
+function getPropertiesKeyData(sourceData) {
+	const { type, description } = sourceData
+
+	// 其他参数处理
+	const pushData: ObjectMap = {}
+	if (sourceData.enum) {
+		pushData.enum = sourceData.enum
+	}
+	if (description) {
+		pushData.description = description
+	}
+
+	return { ...pushData, type: apiTypeMap[type] }
+}
+
+function refMethod(item: { apiData: any; routeString: string }): ObjectMap {
+	const { apiData, routeString } = item
+	const dataA = routeString.split('#/')
+	const dataB = dataA?.[1]?.split('/')
+	const dataC = getArrayReduceObject(apiData, dataB || []) || {}
+	return dataC
+}
+
+function errLog(item) {
+	console.error(item)
+}
+
+function getRequestBodyData(item: { targetData: any; apiData: any }) {
+	const { targetData = {}, apiData } = item
+	const { requestBody } = targetData?.data || {}
+	// console.log('requestBody', requestBody, apiData)
+	const { content } = requestBody
+	let bodyTypeData = {}
+	for (const forItem in requestBodyType) {
+		const { route, method } = requestBodyType[forItem]
+		if (content[forItem]) {
+			const typeData = getArrayReduceObject(content[forItem], route)
+			if (!isTrue(typeData)) {
+				errLog('getRequestBodyData  requestBodyType，没有找到对应的参数')
+				return {}
+			}
+			bodyTypeData = method({ routeString: typeData, apiData })
+		}
+	}
+
+	console.log('bodyTypeData', bodyTypeData)
 }
--- src/views/knife4j/index.tsx
+++ src/views/knife4j/index.tsx
@@ -1,6 +1,6 @@
 import { defineComponent } from 'vue'
 import Axios from '@/api'
-import { setKnife4jApiData } from '@/views/knife4j/_module'
+import { getTargetApiData, setKnife4jApiData } from '@/views/knife4j/_module'
 
 export default defineComponent({
 	name: 'knife4j',
@@ -8,7 +8,8 @@ export default defineComponent({
 		Axios.get('https://supply-gateway-test.htwig.com/scm/v3/api-docs').then((res: any) => {
 			const { data } = res
 			const pathList = setKnife4jApiData({ data })
-			console.log(pathList)
+			const testData = { tag: '扣款单', path: '/scm/scm/deductOrder/addDeductOrder', methods: 'post' }
+			getTargetApiData({ pathList: pathList, targetItem: testData, apiData: data })
 		})
 		return () => <div>1</div>
 	}
